5e1990402ba62639c929f9a60dd057226db70a2b
root user <157661106+rooteek@users.noreply.github.com>
2024-04-13 18:09:37 +0530

    Update App.js

--- ConvertCases/src/App.js
+++ ConvertCases/src/App.js
@@ -8,29 +8,44 @@ import Alert from './components/Alert';
 
 function App() {
 
- const[mode,setMode]= useState('light'); 
- const toggleMode=()=>{
-  if(mode==='dark'){
-    setMode('light');
-    document.body.style.backgroundColor='white';
-    console.log(mode)
-  }
-  else{
-    setMode('dark');
-    document.body.style.backgroundColor='black'
-    console.log(mode)
+  const [mode, setMode] = useState('light');
 
+  const [alert,setAlert]=useState(null);
+
+  const showAlert=(massage,type)=>{
+    setAlert({
+      msg:massage,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
   }
- }
+
+  const toggleMode = () => {
+    if (mode === 'dark') {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      console.log(mode);
+      showAlert("Ligth Mord Enable","success");
+    }
+    else {
+      setMode('dark');
+      document.body.style.backgroundColor = 'black'
+      console.log(mode)
+      showAlert("Dark Mord Enable","success");
+
+    }
+  }
+
 
   return (
     <>
-      <Navbar title="ReactProject" about="AboutSection" home="TextForm" Action="Number" mode={mode} toggleMode={toggleMode}/>
-      <Alert/>
-      <div className='container'>  
-
-      <Textfomr heading="Enter Text" mode={mode}/>
-      {/* <About/> */}
+      <Navbar title="ReactProject" about="AboutSection" home="TextForm" Action="Number" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className='container'>
+        <Textfomr heading="Enter Text" mode={mode} showAlert={showAlert}/>
+        {/* <About/> */}
       </div>
     </>
   );
